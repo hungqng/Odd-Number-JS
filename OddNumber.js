c897daabbca7e1c137d6422f0146fb5e3007450f
hungqng <97866339+hungqng@users.noreply.github.com>
2022-02-10 13:30:47 -0800

    Add files via upload
    Solution Hackerrank practice

---
+++ OddNumber.js
@@ -0,0 +1,12 @@
+// Insert variables
+var l = 2;
+var r = 5;
+
+var i = 0;
+    var answer = [];
+    for (i=l; i<=r; i++){
+        if ((i % 2) != 0){
+            answer.push(i)
+        }
+    }
+    return answer
